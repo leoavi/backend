acc3663a39cc0f26eeba25b332b4fd97498044fa
leoavi <leoavivideos@gmail.com>
2020-05-04 09:40:52 -0300

    Update index.js
    ..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -8,9 +8,30 @@ app.use(express.json());
 
 const projects = [];
 
+function logRequests(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "Invalid project id " });
+  }
+
+  return next();
+}
+
 app.get("/projects", (req, res) => {
   const { title } = req.query;
 
+  // find
   const results = title
     ? projects.filter((project) => project.title.includes(title))
     : projects;
@@ -23,15 +44,17 @@ app.post("/projects", (req, res) => {
 
   const project = { id: uuid(), title, owner };
 
+  // insert
   projects.push(project);
 
   return res.json(project);
 });
 
-app.put("/projects/:id", (req, res) => {
+app.put("/projects/:id", validateProjectId, (req, res) => {
   const { id } = req.params;
   const { title, owner } = req.body;
 
+  // edit
   const projectIndex = projects.findIndex((project) => project.id === id);
 
   if (projectIndex < 0) {
@@ -49,9 +72,10 @@ app.put("/projects/:id", (req, res) => {
   return res.json(project);
 });
 
-app.delete("/projects/:id", (req, res) => {
+app.delete("/projects/:id", validateProjectId, (req, res) => {
   const { id } = req.params;
 
+  // delete
   const projectIndex = projects.findIndex((project) => project.id === id);
 
   if (projectIndex < 0) {
