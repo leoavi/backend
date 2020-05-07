6bbd21b555f2c1fc5b028dd51d95299b97c75ea9
leoavi <leoavivideos@gmail.com>
2020-05-07 11:52:06 -0300

    implementação cors
    implementação cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+const cors = require("cors");
+
 const express = require("express");
 
 const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
